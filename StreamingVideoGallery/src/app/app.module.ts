6e82bfb10657844e80e19727a46959a31ebaf296
Michael Albanese <38363373+MichaelAlbanese@users.noreply.github.com>
2018-08-12 20:33:01 -0500

    Add files via upload

---
+++ StreamingVideoGallery/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { VgBufferingModule } from 'videogular2/buffering';
+import { VgControlsModule } from 'videogular2/controls';
+import { VgCoreModule } from 'videogular2/core';
+import { VgOverlayPlayModule } from 'videogular2/overlay-play';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
